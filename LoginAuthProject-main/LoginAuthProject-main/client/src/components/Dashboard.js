5223cb34f6221f0d4efca4cfe6f8a7bb392a23f3
nag <131885440+nagababu9143@users.noreply.github.com>
2023-11-26 10:27:33 +0530

    Authentication to Client

---
+++ LoginAuthProject-main/LoginAuthProject-main/client/src/components/Dashboard.js
@@ -0,0 +1,64 @@
+import React, { useState,useEffect,useContext} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { LoginContext } from './contextprovider/context';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+const Dashboard = () => {
+    const [data, setData] = useState(false);
+    const {LoginData,setLoginData} = useContext(LoginContext);
+    //console.log(LoginData.ValidUserOne.fname)
+    const fname = LoginData?.ValidUserOne?.fname ?? '';
+    const navigate = useNavigate();
+
+    const dashboardValid = async () => {
+        let token = localStorage.getItem('usersdatatoken');
+        //console.log(token);
+
+        const res = await fetch("/validuser", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        });
+
+        const data = await res.json();
+        //console.log(data);
+        if (data.status === 401 || !data) {
+            navigate("*");
+            //console.log("error");
+        } else {
+            console.log("user verified");
+            setLoginData(data)
+            navigate("/dash");
+        }
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            dashboardValid();
+            setData(true)
+        }, 2000)
+
+    }, [])
+
+    return (
+        <>
+            {data ?
+                <>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", textDecoration:"none"}}>
+                    <img src="./man.png" style={{ width: "200px", marginTop: 20 }} alt="" />
+                    <h1>Welcome, {fname ? fname.toUpperCase() : ''}</h1>
+                </div></>
+                :
+                <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh" }}>
+                    Loading... &nbsp;
+                    <CircularProgress />
+                </Box>
+            }
+        </>
+    )
+}
+
+export default Dashboard
